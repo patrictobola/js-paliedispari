be7bb55fa330bcc3f119b881b979f4825a5a4833
patrictobola <tobola92@gmail.com>
2023-06-06 15:17:52 +0200

    changed js file name

--- js/palindrome.js
+++ js/palindrome.js
@@ -6,9 +6,8 @@ const palindromeBtn = document.querySelector('#palindrome button');
 
 // Creo una funzione che stabilisca se la parola inserita dall'utente è palindroma 
 function isPalindrome(word){
-    const len = word.length;
-    for (let i = 0; i < len / 2; i++){
-        if (word[i] !== word[len - 1 - i]){
+    for (let i = 0; i < word.length / 2; i++){
+        if (word[i] !== word[word.length - 1 - i]){
             return 'Non è palindroma';
         }
     }
