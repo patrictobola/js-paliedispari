24c60b77f71afc702a45fbe62494f978fe088c34
patrictobola <tobola92@gmail.com>
2023-06-06 17:33:25 +0200

    minor fix in palindrome exercise

--- js/palindrome.js
+++ js/palindrome.js
@@ -3,22 +3,25 @@ console.log('JS OK')
 // Recupero dal DOM input e button 
 const palindromeInput = document.querySelector('#palindrome input');
 const palindromeBtn = document.querySelector('#palindrome button');
+const palindromeParagraph = document.querySelector('#palindrome p');
 
 // Creo una funzione che stabilisca se la parola inserita dall'utente è palindroma 
 function isPalindrome(word){
     for (let i = 0; i < word.length / 2; i++){
         if (word[i] !== word[word.length - 1 - i]){
-            return 'Non è palindroma';
+            return 'Ops, non è palindroma';
         }
     }
-    return 'è palindroma';
+
+    return 'Daje è palindroma';
     
 }
 
 // Creo un event listener che raccoglie il value dell'input dove l'utente inserisce la parola 
 palindromeBtn.addEventListener('click', function(){
-    word = palindromeInput.value;
+    const word = palindromeInput.value;
     const result = isPalindrome(word);
     console.log(result)
+    palindromeParagraph.innerHTML = result
 })
 
