9493078c290090ba995c293bd01624dad72ad6d9
patrictobola <tobola92@gmail.com>
2023-06-07 10:06:59 +0200

    another minor fix

--- js/palindrome.js
+++ js/palindrome.js
@@ -7,6 +7,7 @@ const palindromeParagraph = document.querySelector('#palindrome p');
 
 // Creo una funzione che stabilisca se la parola inserita dall'utente è palindroma 
 function isPalindrome(word){
+    word = word.toLowerCase();
     for (let i = 0; i < word.length / 2; i++){
         if (word[i] !== word[word.length - 1 - i]){
             return false;
@@ -18,7 +19,7 @@ function isPalindrome(word){
 
 // Creo un event listener che raccoglie il value dell'input dove l'utente inserisce la parola 
 palindromeBtn.addEventListener('click', function(){
-    const word = palindromeInput.value.toLowerCase();
+    const word = palindromeInput.value;
     const result = isPalindrome(word) ? 'La parola è palindroma' : 'La parola non è palindroma';
     console.log(result)
     palindromeParagraph.innerHTML = result
